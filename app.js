a86e9ced14328fd5c5c2be76b7eb789c221dc802
tharale <tle12348@terpmail.umd.edu>
2025-05-14 19:30:45 -0400

    adding node.js for one book page

---
+++ app.js
@@ -0,0 +1,78 @@
+// Node.js
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/HomePage.html');
+  });  
+
+app.get('/:bookID', async (req, res) => {
+    const bookID = req.params.bookID;
+    const apiKey = 'AIzaSyDfHBexf1SnirVY0BW3XlXhQ-iIVvhlft8'
+
+    await fetch(`https://www.googleapis.com/books/v1/volumes/${bookID}?key=${apiKey}`)
+    .then((result) => result.json())
+    .then((data) => {
+
+        const book = data.volumeInfo;
+        
+        res.send(`
+        <html>
+            <head>
+                <title>Looks Books</title>
+                <link rel="stylesheet" href="global.css">
+                <script defer src="global.js"></script>
+            </head>
+            <body>
+                <!-- Header -->
+                <header id="daHeader">
+                    <h1>Looks Books</h1>
+                </header>
+                <!-- Navigation Bar -->
+                <nav>
+                    <ul id="navBar">
+                        <li><a href = "HomePage.html">Home Page</a></li>
+                        <li><a href = "BookPage.html">Book Page</a></li>
+                        <li><a href = "AboutPage.html">About Us</a></li>
+                        <li>
+                            <!-- Search Bar -->
+                            <form onsubmit="searchBooksGoogle(event)" id="searchForm">
+                                <input type="text" id="searchBox">
+                                <button type="submit" id="searchSubmit" class="button-11">Submit</button>
+                            </form>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- One Book Page -->
+                <div id="oneBookLeftSide">
+                    <img src="http://books.google.com/books/publisher/content?id=${bookID}&printsec=frontcover&img=1&
+                            zoom=1&imgtk=AFLRE72eeeOuMSuFAq092-LhG39YKvUY7kkHzcSZV2scVd0_cL1CrlRHJIBirqdqJQvZfMJr8n73
+                            eqbXEL2iTbdukRcn6jNmaG86FPcv2ccHuy1ZRNgJtmoncqEb9crXw2D6d3WZWtUS&source=gbs_api" 
+                            width="250" height="380">
+                    <div id="oneBookRightSide">
+                        <h2>${book.title}</h2>
+                        <p style="font-size: 18px;">${book.averageRating ? book.averageRating.toFixed(1) + ' Rating':'No Ratings'}</p>
+                        <h4>Description:</h4>
+                        <p style="font-size: 12px; font-family: calibri;">${book.description}</p>
+                    </div>
+                </div>
+                
+                <!-- Search Results -->
+                <div id="searchResultsContainer">
+                    <table id="resultsTable"></table>
+                </div>
+                
+            </body>
+        </html>
+        `)
+    });
+});
+
+app.listen(port, () => {
+    console.log('App is alive on port', port)
+});
---
+++ public/global.js
@@ -0,0 +1,107 @@
+// Open Library Function (not being used)
+
+async function searchBooks(event) {
+    event.preventDefault();
+
+    const welcomeMessage = document.getElementById('welcomeMessage');
+    welcomeMessage.innerHTML = ''
+
+    const resultsTable = document.getElementById('resultsTable');
+    resultsTable.innerHTML = '';
+
+    const bookName = document.getElementById('searchBox').value.trim().replace(/\s+/g, "+");
+    console.log(bookName);
+    
+    
+    await fetch(`https://openlibrary.org/search.json?title=${bookName}`)
+    .then((result) => result.json())
+    .then((data) => {
+        const firstRow = document.createElement('tr');
+
+        const coverColumn = document.createElement('th');
+        coverColumn.textContent = 'Cover';
+        firstRow.appendChild(coverColumn);
+
+        const titleColumn = document.createElement('th');
+        titleColumn.textContent = 'Title';
+        firstRow.appendChild(titleColumn);
+
+        const authorColumn = document.createElement('th');
+        authorColumn.textContent = 'Author';
+        firstRow.appendChild(authorColumn);
+
+        resultsTable.append(firstRow);
+
+        const books = data.docs;
+        books.forEach(book => {
+            const tableRow = document.createElement('tr');
+
+            const coverCell = document.createElement('td');
+            coverCell.innerHTML = `<img src="https://covers.openlibrary.org/b/id/${book.cover_i}-S.jpg" width="40" height="60">`;
+            tableRow.appendChild(coverCell);
+
+            const titleCell = document.createElement('td');
+            titleCell.textContent = book.title;
+            titleCell.setAttribute('class', 'titleCell')
+            tableRow.appendChild(titleCell);
+
+            const authorCell = document.createElement('td');
+            authorCell.textContent = book.author_name;
+            tableRow.appendChild(authorCell);
+
+            resultsTable.append(tableRow);
+        });
+
+        // im a genius
+        resultsTable.setAttribute('class', 'styleResultsTable')
+    });
+};
+
+// Google API Function (being used)
+
+async function searchBooksGoogle(event) {
+    event.preventDefault();
+
+    const apiKey = 'AIzaSyDfHBexf1SnirVY0BW3XlXhQ-iIVvhlft8'
+
+    const welcomeMessage = document.getElementById('welcomeMessage');
+    welcomeMessage.innerHTML = ''
+
+    const resultsTable = document.getElementById('resultsTable');
+    resultsTable.innerHTML = '';
+
+    const bookName = document.getElementById('searchBox').value.trim().replace(/\s+/g, "+");
+    console.log(bookName);
+    
+    
+    await fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookName}&download=epub&key=${apiKey}`)
+    .then((result) => result.json())
+    .then((data) => {
+
+        const books = data.items;
+        books.forEach(book => {
+            const tableRow = document.createElement('tr');
+
+            const coverCell = document.createElement('td');
+            coverCell.innerHTML = `<img src="https://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=5&source=gbs_api" 
+                                width="40" height="60">`;
+            tableRow.appendChild(coverCell);
+
+            const titleAuthorCell = document.createElement('td');
+            titleAuthorCell.innerHTML = `<strong><a href="/${book.id}" class="bookLink">
+                                        ${book.volumeInfo.title}</a></strong><br>By: ${book.volumeInfo.authors}`
+            titleAuthorCell.setAttribute('class', 'titleAuthorCell')
+            tableRow.appendChild(titleAuthorCell);
+
+            const authorCell = document.createElement('td');
+            authorCell.innerHTML = `<strong>Average Rating:</strong> ${book.volumeInfo.averageRating}<br>
+                                    Published: ${book.volumeInfo.publishedDate}`
+            tableRow.appendChild(authorCell);
+
+            resultsTable.append(tableRow);
+        });
+
+        // im a genius
+        resultsTable.setAttribute('class', 'styleResultsTable')
+    });
+};
